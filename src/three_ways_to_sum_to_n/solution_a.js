4f3bf336c43599ecefc8f1188b5cfd717a5433f4
Jasmine Chen <jasminechenyuqinggg@gmail.com>
2021-03-05 17:31:41 +0800

    1st qn done

--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -3,6 +3,10 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+var sum_to_n = function (n) {
+  var results = 0;
+  for (var i = 0; i <= n; i++) {
+    results += i;
+  }
+  return results;
 };
